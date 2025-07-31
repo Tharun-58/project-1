530b3c2d46acdc024b761bb2cfbba037f6414c0e
Tharun-58 <24bct058@kgcas.com>
2025-07-31 08:44:55 +0530

    Add files via upload

---
+++ day-1__task-1/script.js
@@ -0,0 +1,14 @@
+console.log("=== Task 1: Variables ===");
+var name = "John";
+let age = 28;
+const city = "Chicago";
+
+console.log("Name (var):", name);
+console.log("Age (let):", age);
+console.log("City (const):", city);
+
+name = "J.Smith";
+age = 29;
+
+console.log("Modified Name:", name);
+console.log("Modified Age:", age);
